626af1c0e45d3bb07aa87e3f771608248b2e979c
Dwayne Codling <dwayneandrecodling@gmail.com>
2016-04-03 14:56:23 +0100

    created splash screen opener

--- app/js/main.js
+++ app/js/main.js
@@ -25,3 +25,16 @@ $(function(){
     $(this).addClass("active");
 });
 });
+
+// Intro Full Screen page Section  
+
+
+(function() {
+        var windowH = $(window).height(),
+          introEl = $('div.opener'),
+          introHeadingH = introEl.find('h1').height();
+        introEl.css('padding', (windowH - introHeadingH)/2 + 'px 0');
+        $(document).on('scroll', function() {
+          introEl.slideUp(1000, function() { $(document).off('scroll'); });
+        });
+      })();
