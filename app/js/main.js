f1a5d90aed42026417254620fb3557e635b76255
Dwayne Codling <dwayneandrecodling@gmail.com>
2016-05-11 16:06:08 +0100

    added scroll magic and parallax js

--- app/js/main.js
+++ app/js/main.js
@@ -311,6 +311,16 @@ jQuery(document).ready(function($){
 
 
 
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+        duration: 175,  // the scene should last for a scroll distance of 100px
+        offset: 220      // start this scene after scrolling for 50px
+    })
+    .setPin(".header") // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
 
 
  
