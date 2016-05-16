9dda7baeb32534ae6ce23b15c1c3b33b20b6d564
Dwayne Codling <dwayneandrecodling@gmail.com>
2016-05-16 16:04:00 +0100

    update lates local version

--- app/js/main.js
+++ app/js/main.js
@@ -300,7 +300,7 @@ var controller = new ScrollMagic.Controller();
 // create a scene
 new ScrollMagic.Scene({
         duration: 480,  // the scene should last for a scroll distance of 100px
-        offset: 300      // start this scene after scrolling for 50px
+        offset: 280      // start this scene after scrolling for 50px
     })
     .setPin(".welcome-div") // pins the element for the the scene's duration
     .addTo(controller); // assign the scene to the controller
