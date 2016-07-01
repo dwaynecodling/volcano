5ee88030680371f5e7a9756336d153fab3013f69
Dwayne Codling <dwayneandrecodling@gmail.com>
2016-07-01 16:22:31 +0100

    updated jquery 3.0

--- app/js/main.js
+++ app/js/main.js
@@ -350,7 +350,7 @@ animation: 'fade', autoplay: true, speed: 4000, delay: 9000, arrows: false, nav:
 
 
 
-$(window).load( function(){
+$(window).on('load', function() {
   $('.services-header').animateCSS('fadeInUp');
 
     $('.services-list').animateCSS('fadeInUp', {delay:500});
