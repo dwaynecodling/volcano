7bad0d713607130edad6816d08b7915162256ff3
Rudybutler <rudybutler@Rudys-MacBook-Pro.local>
2016-08-16 13:13:47 +0100

    test changes to php and contact form

--- dist/action_page.php
+++ dist/action_page.php
@@ -1,29 +1,84 @@
-<?php
-// if the url field is empty
-if(isset($_POST['url']) && $_POST['url'] == ''){
-	// put your email address here
-	$youremail = 'dwayneandrecodling@gmail.com';
-	// prepare a "pretty" version of the message
-	// Important: if you added any form fields to the HTML, you will need to add them here also
+?>
 
-	$body = "This is the form that was just submitted:
-	Name:  $_POST[name]
-	E-Mail: $_POST[email]
-	Telephone: $_POST[tel]
-	Services: $_POST[services]
-	Message: $_POST[details]";
-	// Use the submitters email if they supplied one
-	// (and it isn't trying to hack your form).
-	// Otherwise send from your email address.
-	if( $_POST['email'] && !preg_match( "/[\r\n]/", $_POST['email']) ) {
-	  $headers = "From: $_POST[email]";
-	} else {
-	  $headers = "From: $youremail";
-	}
-	// finally, send the message
-	mail($youremail, 'Contact Form', $body, $headers );
+<?php // Initialize variables to null.
+$services =""; // Sender Name
+$name =""; // Sender's email ID
+$tel =""; // Subject of mail
+$email =""; // Sender's Message
+$servicesError ="";
+$nameError ="";
+$telError ="";
+$emailError ="";
+$successMessage =""; // On submittingform below function will execute.
+if(isset($_POST['submit'])) { // Checking null values in message.
+if (empty($_POST["services"])){
+$servicesError = "What services you are interested in";
 }
-// otherwise, let the spammer think that they got their message through
+else
+ {
+$name = test_input($_POST["services"]); // check name only contains letters and whitespace
+if (!preg_match("/^[a-zA-Z ]*$/",$services))
+{
+$servicesError = "Sorry, only letters and spaces are allowed";
+}
+} // Checking null values inthe message.
+if (empty($_POST["name"]))
+{
+$nameError = "Please provide us with a contact name";
+}
+else
+ {
+$name = test_input($_POST["name"]);
+} // Checking null values inmessage.
+if (empty($_POST["tel"]))
+{
+$telError = "Contact number required";
+}
+else
+{
+$tel = test_input($_POST["tel"]);
+} // Checking null values inmessage.
+if (empty($_POST["email"]))
+{
+$emailError = "Valid email required";
+}
+else
+ {
+$email = test_input($_POST["email"]);
+} // Checking null values inthe message.
+if( !($services=='') && !($name=='') && !($tel=='') &&!($email=='') )
+{ // Checking valid email.
+if (preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+{
+$header= $name."<". $email .">";
+  $headers = "From: $youremail";
+$msg = "Hello! $name Thank you...! For Contacting Us.
+Name: $name
+E-mail: $email
+Purpose: $purpose
+Message: $message
+This is a Contact Confirmation mail. We Will contact You as soon as possible.";
+$msg1 = " $name Contacted Us. Hereis some information about $name.
+Name: $name
+E-mail: $email
+Purpose: $purpose
+Message: $message "; /* Send the message using mail() function */
+if(mail($email, $headers, $msg ) && mail("dwayneandrecodling@gmail.com", $header, $msg1 ))
+{
+$successMessage = "Message sent successfully.......";
+}
+}
+else
+{ $emailError = "Invalid Email";
+ }
+ }
+} // Function for filtering input values.function test_input($data)
+{
+$data = trim($data);
+$data =stripslashes($data);
+$data =htmlspecialchars($data);
+return $data;
+}
 ?>
 
 
