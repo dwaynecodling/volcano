7bc4f7f25b912616cee1a03abc187fa258f402b0
Dwayne Codling <dwayneandrecodling@gmail.com>
2016-08-23 13:04:46 +0100

    revert changes

--- app/js/main.js
+++ app/js/main.js
@@ -377,6 +377,12 @@ jQuery(document).ready(function() {
 });
 
 
+//Submit Form
+
+
+    // split your email into two parts and remove the @ symbol
+    var first = "dwayneandrecodling";
+    var last = "gmail.com";
 
 
 
@@ -404,68 +410,3 @@ jQuery(document).ready(function($){
       }
     });
 });
-
-
-
-//Submit Form
-
-
-    // split your email into two parts and remove the @ symbol
-    var first = "dwayneandrecodling";
-    var last = "gmail.com";
-
-
-
-$("#form").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
-    }
-});
-
-
-function submitForm(){
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: "action_page.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
-        }
-    });
-}
-
-function formSuccess(){
-    $("#form")[0].reset();
-    submitMSG(true, "Message Submitted!")
-}
-
-function formError(){
-    $("#form").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg){
-    if(valid){
-        var msgClasses = "h3 text-center tada animated text-success";
-    } else {
-        var msgClasses = "h3 text-center text-danger";
-    }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-}
